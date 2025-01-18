050d6a43fd50127a5a3bda29e0b6c66df436b416
mahamfathy <mahafathyh@gmail.com>
2025-01-19 00:46:58 +0200

    [style] auth style: edits in styles

--- src/app/feature/auth/components/auth-dynamic-form/auth-dynamic-form.component.ts
+++ src/app/feature/auth/components/auth-dynamic-form/auth-dynamic-form.component.ts
@@ -1,34 +1,35 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { authFormConfig } from '../auth-form-config/auth-form-config';
+import { Component, Input, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { authFormConfig } from "../auth-form-config/auth-form-config";
 
 @Component({
-  selector: 'app-auth-dynamic-form',
-  templateUrl: './auth-dynamic-form.component.html',
-  styleUrl: './auth-dynamic-form.component.scss',
+  selector: "app-auth-dynamic-form",
+  templateUrl: "./auth-dynamic-form.component.html",
+  styleUrl: "./auth-dynamic-form.component.scss",
 })
 export class AuthDynamicFormComponent implements OnInit {
-  @Input({ required: true }) formType: string = '';
+  @Input({ required: true }) formType: string = "";
+  imagePath: string = "assets/images/svg/auth-image.svg";
   inputs: any[] = [];
   authForm: FormGroup = new FormGroup({});
   ngOnInit(): void {
     this.inputs = authFormConfig[this.formType];
     const formGroup: any = {};
     this.inputs.forEach((input) => {
-      if (input.type === 'email') {
-        formGroup[input.control] = new FormControl('', [
+      if (input.type === "email") {
+        formGroup[input.control] = new FormControl("", [
           Validators.required,
           Validators.email,
         ]);
-      } else if (input.type === 'password') {
-        formGroup[input.control] = new FormControl('', [
+      } else if (input.type === "password") {
+        formGroup[input.control] = new FormControl("", [
           Validators.required,
           Validators.pattern(
-            '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$'
+            "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$"
           ),
         ]);
       } else {
-        formGroup[input.control] = new FormControl('', Validators.required);
+        formGroup[input.control] = new FormControl("", Validators.required);
       }
     });
     this.authForm = new FormGroup(formGroup);
