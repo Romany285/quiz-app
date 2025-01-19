5e7a1b07d3a46f416a0d33a47372ab623cf1f8ef
romany <romanyghttas332@gmail.com>
2025-01-19 16:55:08 +0200

    [feay] auth module : create change password componenet

--- src/app/feature/auth/auth-routing.module.ts
+++ src/app/feature/auth/auth-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ResetRequestPasswordComponent } from './components/reset-request-password/reset-request-password.component';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
 
 const routes: Routes = [
   {
     path: 'reset-request-password',
     component: ResetRequestPasswordComponent,
+  },
+  {
+    path: 'change-password',
+    component: ChangePasswordComponent,
   },
 ];
 
