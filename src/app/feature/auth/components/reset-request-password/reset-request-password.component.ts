87bc99ebf16a7472423220f7e6104b3141b77f85
mahamfathy <mahafathyh@gmail.com>
2025-01-19 13:47:05 +0200

    [feat] reset request password: finish the component

--- src/app/feature/auth/components/reset-request-password/reset-request-password.component.ts
+++ src/app/feature/auth/components/reset-request-password/reset-request-password.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-reset-request-password',
-  templateUrl: './reset-request-password.component.html',
-  styleUrl: './reset-request-password.component.scss',
+  selector: "app-reset-request-password",
+  templateUrl: "./reset-request-password.component.html",
+  styleUrl: "./reset-request-password.component.scss",
 })
 export class ResetRequestPasswordComponent {
-  formType: string = 'resetRequestPassword';
+  formType: string = "resetRequestPassword";
+  formTitle: string = "Reset Password";
+  buttonName: string = "Reset";
 }
