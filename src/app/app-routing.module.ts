3db904ef9432d6fe0c861082cfdd48e11c978fc9
romany <romanyghttas332@gmail.com>
2025-01-19 16:45:42 +0200

    [feat] shared module : crate not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: 'auth', loadChildren: () => import('./feature/auth/auth.module').then(m => m.AuthModule) }
+  { path: 'auth', loadChildren: () => import('./feature/auth/auth.module').then(m => m.AuthModule) },
+  {path:'**',component:NotFoundComponent,title:'Not Found Page'}
 ];
 
 @NgModule({
