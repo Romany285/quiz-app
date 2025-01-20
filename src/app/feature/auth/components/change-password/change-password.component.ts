fe8f9bcc638076b473af781df11d2e88ea3177eb
mahamfathy <mahafathyh@gmail.com>
2025-01-20 17:00:20 +0200

    [feat] handle errors :handle errors in the whole app

--- src/app/feature/auth/components/change-password/change-password.component.ts
+++ src/app/feature/auth/components/change-password/change-password.component.ts
@@ -1,30 +1,32 @@
-import { Component } from '@angular/core';
-import { AuthService } from '../../services/auth.service';
-import { ToastrService } from 'ngx-toastr';
-import { Router } from '@angular/router';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { ToastrService } from "ngx-toastr";
+import { AuthService } from "../../services/auth.service";
 
 @Component({
-  selector: 'app-change-password',
-  templateUrl: './change-password.component.html',
-  styleUrl: './change-password.component.scss'
+  selector: "app-change-password",
+  templateUrl: "./change-password.component.html",
+  styleUrl: "./change-password.component.scss",
 })
 export class ChangePasswordComponent {
-  constructor(private _AuthService:AuthService,private _toastrService:ToastrService,private _router:Router){}
-  formType: string = 'changePassword';
+  constructor(
+    private _AuthService: AuthService,
+    private _toastrService: ToastrService,
+    private _router: Router
+  ) {}
+  formType: string = "changePassword";
   formTitle: string = "Change Password";
   buttonName: string = "Change";
-  resMessage = '';
-  changePassword(data:any){
+  resMessage = "";
+  changePassword(data: any) {
     this._AuthService.changePassword(data).subscribe({
       next: (res) => {
         this.resMessage = res.message;
-      }, error: (err) => {
-        console.log(err);
-        this._toastrService.error(err.error.message, 'Error!')
-      }, complete: () => {
+      },
+      complete: () => {
         this._toastrService.success(this.resMessage);
-        this._router.navigate(['/dashboard/home'])
-      }
-    })
+        this._router.navigate(["/dashboard/home"]);
+      },
+    });
   }
 }
