a741bf566b502b6174aeb8e4cc339523692bd584
mahamfathy <mahafathyh@gmail.com>
2025-01-21 01:57:57 +0200

    [feat] student service: create the service

---
+++ src/app/feature/dashboard/modules/instructor/modules/students/services/students.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { IStudent } from "../interfaces/student.interface";
+
+@Injectable({
+  providedIn: "root",
+})
+export class StudentsService {
+  constructor(private _HttpClient: HttpClient) {}
+
+  getAllStudents(): Observable<IStudent[]> {
+    return this._HttpClient.get<IStudent[]>("student");
+  }
+}
