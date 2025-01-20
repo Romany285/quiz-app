ec85556f42ca9df85a384a4f43d8e15a80010aa8
romany <romanyghttas332@gmail.com>
2025-01-20 16:41:53 +0200

    [feat] auth module : add observable to functiots

--- src/app/feature/auth/services/auth.service.ts
+++ src/app/feature/auth/services/auth.service.ts
@@ -6,6 +6,7 @@ import { IRequestResetPassword } from "../interfaces/IRequestResetPassword";
 import { IResetPassword } from "../interfaces/IResetPassword";
 import { IChangePassword } from "../interfaces/IChangePassword";
 import { ILogin } from "../interfaces/ILogin";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: "root",
@@ -13,19 +14,19 @@ import { ILogin } from "../interfaces/ILogin";
 export class AuthService {
   constructor(private _httpClient: HttpClient) {}
 
-  login(data: ILogin) {
+  login(data: ILogin) :Observable<IResLogin>{
     return this._httpClient.post<IResLogin>("auth/login", data);
   }
-  register(data: IRegister) {
+  register(data: IRegister) :Observable<IAuthRes>{
     return this._httpClient.post<IAuthRes>("auth/register", data);
   }
-  forgetPassword(data: IResetPassword){
+  forgetPassword(data: IResetPassword):Observable<IAuthRes>{
     return this._httpClient.post<IAuthRes>("auth/forgot-password", data);
   }
-  resetPassword(data: IRequestResetPassword) {
+  resetPassword(data: IRequestResetPassword):Observable<IAuthRes> {
     return this._httpClient.post<IAuthRes>("auth/reset-password", data);
   }
-  changePassword(data: IChangePassword) {
+  changePassword(data: IChangePassword):Observable<IAuthRes> {
     return this._httpClient.post<IAuthRes>("auth/change-password", data);
   }
   logout() {
