1b93ea8e744361daf1ab4d2341b06906ca873a59
mahamfathy <mahafathyh@gmail.com>
2025-01-20 23:13:01 +0200

    Added interceptors directory and its contents

---
+++ src/app/core/interceptors/global/global.interceptor.ts
@@ -0,0 +1,13 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+
+export const globalInterceptor: HttpInterceptorFn = (req, next) => {
+  const baseUrl = 'https://upskilling-egypt.com:3005/api/'
+  const token = localStorage.getItem('token');
+  const newRequest = req.clone({
+    url: baseUrl + req.url,
+    setHeaders: {
+      Authorization: token ? token : '',
+    },
+  })
+  return next(newRequest);
+};
