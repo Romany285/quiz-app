a4c8021d7b73fc14517d37d2a5f8f45a348c5d46
mahamfathy <mahafathyh@gmail.com>
2025-01-21 01:57:19 +0200

    [feat] instructor module: create the module

--- src/app/feature/dashboard/modules/instructor/instructor-routing.module.ts
+++ src/app/feature/dashboard/modules/instructor/instructor-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InstructorComponent } from './instructor.component';
 
-const routes: Routes = [{ path: '', component: InstructorComponent }];
+const routes: Routes = [{ path: '', component: InstructorComponent }, { path: 'students', loadChildren: () => import('./modules/students/students.module').then(m => m.StudentsModule) }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
