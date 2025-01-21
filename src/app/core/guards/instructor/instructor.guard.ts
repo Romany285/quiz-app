6ead8f21c8769a1137b3190c4a445bc33aba2882
romany <romanyghttas332@gmail.com>
2025-01-21 02:58:56 +0200

    [feat] core : edit guards

--- src/app/core/guards/instructor/instructor.guard.ts
+++ src/app/core/guards/instructor/instructor.guard.ts
@@ -5,7 +5,7 @@ import { AuthService } from '../../../feature/auth/services/auth.service';
 export const instructorGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const authService = inject(AuthService);
-  if (localStorage.getItem('token') !== null && authService.role === 'instructor') {
+  if (localStorage.getItem('token') !== null && authService.role === 'Instructor') {
     return true;
   }
   router.navigate(['/instructor']);
--- src/app/core/guards/student/student.guard.ts
+++ src/app/core/guards/student/student.guard.ts
@@ -5,7 +5,7 @@ import { AuthService } from '../../../feature/auth/services/auth.service';
 export const studentGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
   const authService = inject(AuthService);
-  if (localStorage.getItem('token') !== null && authService.role === 'student') {
+  if (localStorage.getItem('token') !== null && authService.role === 'Student') {
     return true;
   }
   router.navigate(['/student']);
