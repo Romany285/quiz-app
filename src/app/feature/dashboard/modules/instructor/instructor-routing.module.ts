a909e3c37b6b1a771a7549a2a61265638b6435aa
romany <romanyghttas332@gmail.com>
2025-01-21 17:37:24 +0200

    margge

--- src/app/feature/dashboard/modules/instructor/instructor-routing.module.ts
+++ src/app/feature/dashboard/modules/instructor/instructor-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { InstructorComponent } from "./instructor.component";
 
 const routes: Routes = [{ path: '', component: InstructorComponent },
    { path: 'students', loadChildren: () => import('./modules/students/students.module').then(m => m.StudentsModule) }, 
