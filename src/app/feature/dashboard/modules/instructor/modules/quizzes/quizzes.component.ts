dc9ce87862cf7d2b934e2f11c1d878f1447f38fd
mahamfathy <mahafathyh@gmail.com>
2025-01-24 19:44:55 +0200

    [style] header-green-arrow file : create the file

--- src/app/feature/dashboard/modules/instructor/modules/quizzes/quizzes.component.ts
+++ src/app/feature/dashboard/modules/instructor/modules/quizzes/quizzes.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-quizzes',
-  templateUrl: './quizzes.component.html',
-  styleUrl: './quizzes.component.scss'
+  selector: "app-quizzes",
+  templateUrl: "./quizzes.component.html",
+  styleUrl: "./quizzes.component.scss",
 })
 export class QuizzesComponent {
-
+  upcomingQuizzes: any[] = [];
+  completedQuizzes: any[] = [];
+  headers: string[] = ["Title", "Date", "Time", "Number of Students", "Action"];
 }
