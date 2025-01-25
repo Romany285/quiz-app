e8b4b35077ba2681ff9e37d5bbb640dd86605b55
YOUSSEF-RAAFAT-KAMEL <youssefrafat700@gmail.com>
2025-01-25 13:08:45 +0200

    [feat]: add time in nav and fix spaces in child dashboard

--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -1,11 +1,30 @@
-import { Component } from "@angular/core";
+import { Time } from "@angular/common";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-nav",
   templateUrl: "./nav.component.html",
   styleUrl: "./nav.component.scss",
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
   userName = localStorage.getItem("name");
   role = localStorage.getItem("role");
+  currentTime: string = '';
+  private timerInterval: any;
+
+  ngOnInit(): void {
+    this.updateTime(); 
+    this.timerInterval = setInterval(() => this.updateTime(), 1000); 
+  }
+
+  ngOnDestroy(): void {
+    if (this.timerInterval) {
+      clearInterval(this.timerInterval); 
+    }
+  }
+
+  private updateTime(): void {
+    const now = new Date();
+    this.currentTime = now.toLocaleTimeString(); 
+  }
 }
