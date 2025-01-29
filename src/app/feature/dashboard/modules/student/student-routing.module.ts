0d2c02140f9ac11b3892e9accf7a01c82f652bae
romany <romanyghttas332@gmail.com>
2025-01-29 02:54:03 +0200

    [feat] student module : create quizzes component

--- src/app/feature/dashboard/modules/student/student-routing.module.ts
+++ src/app/feature/dashboard/modules/student/student-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { StudentComponent } from "./student.component";
+import { ResultsComponent } from "./components/results/results.component";
+import { QuizzesComponent } from "./components/quizzes/quizzes.component";
 
 const routes: Routes = [
-  { path: "", component: StudentComponent },
   {
     path: "exam",
     loadChildren: () =>
       import("./modules/exams/exams.module").then((m) => m.ExamsModule),
   },
+  {path:'results',component:ResultsComponent},
+  {path:'quizzes',component:QuizzesComponent},
 ];
 
 @NgModule({
--- src/app/feature/dashboard/modules/student/student.module.ts
+++ src/app/feature/dashboard/modules/student/student.module.ts
@@ -3,9 +3,14 @@ import { NgModule } from "@angular/core";
 
 import { StudentRoutingModule } from "./student-routing.module";
 import { StudentComponent } from "./student.component";
+import { JoinCodeComponent } from './components/join-code/join-code.component';
+ 
+import { QuizzesComponent } from './components/quizzes/quizzes.component';
+import { SharedModule } from "../../../../shared/shared.module";
+import { ResultsComponent } from './components/results/results.component';
 
 @NgModule({
-  declarations: [StudentComponent],
-  imports: [CommonModule, StudentRoutingModule],
+  declarations: [StudentComponent, JoinCodeComponent , QuizzesComponent, ResultsComponent],
+  imports: [CommonModule, StudentRoutingModule,SharedModule],
 })
 export class StudentModule {}
