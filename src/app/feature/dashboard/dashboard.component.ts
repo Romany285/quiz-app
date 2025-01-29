6988652d6b41de60cfeea59d751f560d13c50013
mahamfathy <mahafathyh@gmail.com>
2025-01-29 02:41:39 +0200

    edits

--- src/app/feature/dashboard/dashboard.component.ts
+++ src/app/feature/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, OnInit } from "@angular/core";
-import { DashboardService } from './services/dashboard.service';
+import { DashboardService } from "./services/dashboard.service";
 
 @Component({
   selector: "app-dashboard",
@@ -7,13 +7,16 @@ import { DashboardService } from './services/dashboard.service';
   styleUrl: "./dashboard.component.scss",
 })
 export class DashboardComponent implements OnInit {
-  _dashboardService = inject(DashboardService)
+  _dashboardService = inject(DashboardService);
+  isExpanded: boolean = false;
   ngOnInit(): void {
     this._dashboardService.getTopFiveQuizzes().subscribe({
       next: (res) => {
         // console.log(res)
-      }
-    })
+      },
+    });
   }
-
+  toggleSidebar() {
+    this.isExpanded !== this.isExpanded;
+  }
 }
