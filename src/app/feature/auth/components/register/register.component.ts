cba3b2c69e9f4ccbb39ae68b50de71a4e295ea54
mahamfathy <mahafathyh@gmail.com>
2025-01-31 19:29:30 +0200

    [feat] register components  :  add localstorage to the email in login

--- src/app/feature/auth/components/register/register.component.ts
+++ src/app/feature/auth/components/register/register.component.ts
@@ -13,11 +13,12 @@ export class RegisterComponent {
   formType: string = "register";
   formTitle: string = "Create your account and start using QuizWiz!";
   buttonName: string = "Sign Up";
-  constructor(private _AuthService: AuthService, private _Router: Router) {
-    localStorage.clear();
-  }
+  constructor(private _AuthService: AuthService, private _Router: Router) {}
   register(formValue: FormGroup) {
     this._AuthService.register(formValue.value).subscribe({
+      next: (res) => {
+        localStorage.setItem("email", formValue.get("email")?.value);
+      },
       complete: () => {
         this._Router.navigate([""]);
       },
